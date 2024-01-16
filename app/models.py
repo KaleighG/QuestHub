251b92b6c06ab077b32cc66e5606a07a3392504b
Kaleigh Garcia <icytornado13@gmail.com>
2024-01-16 10:33:09 -0600

    setting settings page

--- app/models.py
+++ app/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     role = models.CharField(max_length=200)
-    profile_picture = models.ImageField(upload_to='media', blank=True, null=True)
+    profile_pic = models.ImageField(upload_to='media', blank=True, null=True)
     friends = models.ManyToManyField('self', symmetrical=True)
     bio = models.CharField(max_length=250, null=True, blank=True)
 
